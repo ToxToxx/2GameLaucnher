344c429aad549aabfcc0f831204bd1c6a3f903a3
Matvey <sinema3d00@gmail.com>
2024-07-17 17:04:32 +0300

    making main menu and loading logic

--- Assets/Scripts/RunnerGame/GameLogic/FinishController.cs
+++ Assets/Scripts/RunnerGame/GameLogic/FinishController.cs
@@ -6,6 +6,10 @@ namespace RunnerGame
     {
         [SerializeField] private Canvas _finishScreenCanvas;
 
+        private void Start()
+        {
+            _finishScreenCanvas.gameObject.SetActive(false);
+        }
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.GetComponent<PlayerMovementController>())
