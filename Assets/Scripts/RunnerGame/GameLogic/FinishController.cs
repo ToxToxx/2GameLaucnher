c222115c0eb1355d4873bfb951f5832e8b49f910
Matvey <sinema3d00@gmail.com>
2024-07-17 14:26:56 +0300

    making time controller

--- Assets/Scripts/RunnerGame/GameLogic/FinishController.cs
+++ Assets/Scripts/RunnerGame/GameLogic/FinishController.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
 
-
 namespace RunnerGame
 {
     public class FinishController : MonoBehaviour
     {
+        [SerializeField] private Canvas _finishScreenCanvas;
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.GetComponent<PlayerMovementController>())
             {
                 Time.timeScale = 0f;
+                _finishScreenCanvas.gameObject.SetActive(true);
             }
             
         }
