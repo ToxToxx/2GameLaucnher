b4e269dfaa4385022d40147d222170938629aa2f
Matvey <sinema3d00@gmail.com>
2024-07-17 13:31:24 +0300

    add logic of adding score,get and saving results

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -1,8 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreController : MonoBehaviour
 {
-    private int _score;
+    private const string CLICK_COUNT_SCORE = "ClickCountScore";
+
+    [SerializeField] private Button _clickButton;
+
+    private int _clickCountScore;
+
+    private void Start()
+    {
+        _clickCountScore = PlayerPrefs.GetInt(CLICK_COUNT_SCORE, 0);
+        _clickButton.onClick.AddListener(AddClickScore);
+    }
+
+    private void AddClickScore()
+    {
+        _clickCountScore++;
+        PlayerPrefs.SetInt(CLICK_COUNT_SCORE, _clickCountScore);
+    }
+
+    private void OnApplicationQuit()
+    {
+        PlayerPrefs.SetInt("ClickCount", _clickCountScore);
+    }
+
+    public int GetClickScore()
+    {
+        return _clickCountScore;
+    }
 }
