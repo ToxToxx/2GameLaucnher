c694f8b821a2bd8bcdbf55bd48c0a14d327b53e1
Matvey <sinema3d00@gmail.com>
2024-07-18 11:37:31 +0300

    made asset bundle to load from yandex server

--- Assets/Scripts/AddressablesLogic/LoadAssetsFromServer.cs
+++ Assets/Scripts/AddressablesLogic/LoadAssetsFromServer.cs
@@ -1,58 +1,92 @@
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
-using UnityEngine.ResourceManagement.ResourceProviders;
-using UnityEngine.SceneManagement;
-using System.Collections.Generic;
+using UnityEngine.Networking;
+using System.Collections;
+using TMPro;
 
-public class LoadAssetsFromServer : MonoBehaviour
+public class AssetManager : MonoBehaviour
 {
-    [SerializeField] private Button _loadClickGameAssetsButton;
-    [SerializeField] private Button _loadRunnerGameAssetsButton;
+    // URL до вашего сервера с ассетами (замените на ваш URL)
+    private string assetBundleURL1 = "https://storage.yandexcloud.net/unitytestzadanie/clickgamepackedassets_scenes_all_18adbe70653bfdf222ed6a2188404f33.bundle";
+    private string assetBundleURL2 = "https://storage.yandexcloud.net/unitytestzadanie/runnergamepackedassets_scenes_all_e5fb86d1632af0f0c9e89f935b224b79.bundle";
 
-    private string _scene1Address = "clickgamepackedassets_scenes_all_18adbe70653bfdf222ed6a2188404f33.bundle";
-    private string _scene2Address = "runnergamepackedassets_scenes_all_e5fb86d1632af0f0c9e89f935b224b79.bundle";
+    // Кэшированные AssetBundles
+    private AssetBundle assetBundle1;
+    private AssetBundle assetBundle2;
 
-    private AsyncOperationHandle<SceneInstance> _currentSceneHandle;
+    // UI элементы
+    public Button loadButton1;
+    public Button loadButton2;
+    public Button unloadButton1;
+    public Button unloadButton2;
+    public TextMeshProUGUI statusText;
 
     void Start()
     {
-        _loadClickGameAssetsButton.onClick.AddListener(LoadClickGame);
-        _loadRunnerGameAssetsButton.onClick.AddListener(LoadRunnerGame);
+        // Добавляем слушатели к кнопкам
+        loadButton1.onClick.AddListener(() => LoadAssetBundle(1));
+        loadButton2.onClick.AddListener(() => LoadAssetBundle(2));
+        unloadButton1.onClick.AddListener(() => UnloadAssetBundle(1));
+        unloadButton2.onClick.AddListener(() => UnloadAssetBundle(2));
     }
 
-    void LoadClickGame()
+    // Метод для загрузки ассета
+    public void LoadAssetBundle(int gameNumber)
     {
-        UnloadCurrentScene();
-
-        Addressables.LoadSceneAsync(_scene1Address, LoadSceneMode.Single).Completed += SceneLoadedCallback;
-    }
-
-    void LoadRunnerGame()
-    {
-        UnloadCurrentScene();
-
-        Addressables.LoadSceneAsync(_scene2Address, LoadSceneMode.Single).Completed += SceneLoadedCallback;
-    }
-
-    void SceneLoadedCallback(AsyncOperationHandle<SceneInstance> handle)
-    {
-        if (handle.Status == AsyncOperationStatus.Succeeded)
+        if (gameNumber == 1)
         {
-            _currentSceneHandle = handle;
+            StartCoroutine(DownloadAssetBundle(assetBundleURL1, 1));
+        }
+        else if (gameNumber == 2)
+        {
+            StartCoroutine(DownloadAssetBundle(assetBundleURL2, 2));
+        }
+    }
+
+    // Метод для выгрузки ассета
+    public void UnloadAssetBundle(int gameNumber)
+    {
+        if (gameNumber == 1 && assetBundle1 != null)
+        {
+            assetBundle1.Unload(true);
+            assetBundle1 = null;
+            statusText.text = "AssetBundle 1 выгружен";
+            Debug.Log("AssetBundle 1 выгружен");
+        }
+        else if (gameNumber == 2 && assetBundle2 != null)
+        {
+            assetBundle2.Unload(true);
+            assetBundle2 = null;
+            statusText.text = "AssetBundle 2 выгружен";
+            Debug.Log("AssetBundle 2 выгружен");
+        }
+    }
+
+    // Метод для загрузки ассетбандла
+    private IEnumerator DownloadAssetBundle(string url, int gameNumber)
+    {
+        UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(url);
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            statusText.text = "Ошибка загрузки AssetBundle " + gameNumber;
+            Debug.LogError("Ошибка загрузки: " + request.error);
         }
         else
         {
-            Debug.LogError("Failed to load scene: " + handle.DebugName);
-        }
-    }
-
-    void UnloadCurrentScene()
-    {
-        if (_currentSceneHandle.IsValid())
-        {
-            Addressables.UnloadSceneAsync(_currentSceneHandle);
+            AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(request);
+            if (gameNumber == 1)
+            {
+                assetBundle1 = bundle;
+                statusText.text = "AssetBundle 1 загружен";
+            }
+            else if (gameNumber == 2)
+            {
+                assetBundle2 = bundle;
+                statusText.text = "AssetBundle 2 загружен";
+            }
+            Debug.Log("AssetBundle " + gameNumber + " загружен");
         }
     }
 }
